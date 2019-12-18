34d9ca184f178fa8df3f62a1b39591ddaf207993
tomekgora <tomekgora@gmail.com>
2019-12-18 19:56:34 +0100

    ReadyButton works, no action dispatched

--- src/components/gameroom/ReadyButton.js
+++ src/components/gameroom/ReadyButton.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
 
 class ReadyButton extends Component {
+  state = {
+    userID: null,
+    readyToStart: false,
+  }
+
+  handleClick = () => {
+      if(this.state.readyToStart === false) {
+        this.setState({readyToStart: true})
+      } else {
+        this.setState({readyToStart: false})
+    }
+  };
+
   render() {
     return (
-        <div>   
-        </div>
+          <div>
+            <button onClick={this.handleClick}>
+              {this.state.readyToStart ? (
+                "CLICK TO DELAY START"
+              ) : (
+                "CLICK WHEN READY"
+              )}
+            </button>
+          </div>
     )
   }
 }
