a080396f1c501a681352ad71c788c36f1daa48b6
lottenoorlander <lottenoorlander@gmail.com>
2020-01-28 00:08:58 +0100

    gameboard now shows position of robots

--- src/components/gameroom/GameBoard.js
+++ src/components/gameroom/GameBoard.js
@@ -1,23 +1,64 @@
 import React, { Component } from "react";
+import floorTile from "../../images/tileset/floor/scalable/floor.svg";
+import pitTile from "../../images/tileset/pit/scalable/pit.svg";
+import flag1Tile from "../../images/tileset/checkpoints/scalable/1.svg";
+import flag2Tile from "../../images/tileset/checkpoints/scalable/2.svg";
+import flag3Tile from "../../images/tileset/checkpoints/scalable/3.svg";
+import r1Tile from "../../images/tileset/robot/Twonky/scalable/0.svg";
+import r2Tile from "../../images/tileset/robot/Trundle_Bot/scalable/0.svg";
+import "./GameBoard.css";
 
 class GameBoard extends Component {
+  gameboard = [
+    ["tile", "tile", "tile"],
+    ["tile", "flag1", "tile"],
+    ["pit", "tile", "pit"],
+    ["tile", "tile", "tile"],
+    ["flag2", "tile", "tile"],
+    ["tile", "pit", "tile"],
+    ["tile", "flag3", "tile"]
+  ];
+
+  positionPlayer1x = this.props.currentRoom.users[0].position[0];
+  positionPlayer1y = this.props.currentRoom.users[0].position[1];
+  positionPlayer2x = this.props.currentRoom.users[1].position[0];
+  positionPlayer2y = this.props.currentRoom.users[1].position[1];
+
+  updatedGameBoard = [
+    ...this.gameboard,
+    (this.gameboard[this.positionPlayer1x - 1][this.positionPlayer1y - 1] =
+      "robot1"),
+    (this.gameboard[this.positionPlayer2x - 1][this.positionPlayer2y - 1] =
+      "robot2")
+  ];
   render() {
-    const gameboard = [
-      [t, t, t],
-      [t, t, t],
-      [p, t, p],
-      [t, t, t],
-      [t, t, t],
-      [t, p, t],
-      [t, f, t]
-    ];
     return (
       <div className="board-wrapper">
-        {console.log(this.props.currentRoom)}
+        {console.log("updatedGameBoard", this.updatedGameBoard)}
+        {this.gameboard.map(row =>
+          row.map(tile => {
+            if (tile === "tile") {
+              return <img src={floorTile} className="board-tile" />;
+            } else if (tile === "pit") {
+              return <img src={pitTile} className="board-tile" />;
+            } else if (tile === "flag1") {
+              return <img src={flag1Tile} className="board-tile" />;
+            } else if (tile === "flag2") {
+              return <img src={flag2Tile} className="board-tile" />;
+            } else if (tile === "flag3") {
+              return <img src={flag3Tile} className="board-tile" />;
+            } else if (tile === "robot1") {
+              return <img src={r1Tile} className="board-tile" />;
+            } else if (tile === "robot2") {
+              return <img src={r2Tile} className="board-tile" />;
+            }
+          })
+        )}
+        {/* {console.log(this.props.currentRoom)}
         THIS IS THE GAMEBOARD <br />- each player needs to be assigned a robot{" "}
         <br />- the board needs to be an array <br />- array needs to be updated
         based on current position of both robots when executing move <br />-
-        board needs to be a certain size // remember to look that up
+        board needs to be a certain size // remember to look that up */}
       </div>
     );
   }
